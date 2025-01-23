484f54e6902a6c226cf973349c8ba2ed4725c7ba
dequeirozmarcondes <dequeirozmarcondes@outlook.com>
2025-01-22 21:18:19 -0400

    create UserDTO

---
+++ Application/DTOs/ApplicationUserDTO.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAppBlogApi.Application.DTOs
+{
+    public record ApplicationUserDTO(
+        [Required]
+        [StringLength(100, MinimumLength = 3)]
+        string Name,
+
+        [Required]
+        [StringLength(100, MinimumLength = 3)]
+        string UserName,
+
+        [Required]
+        [EmailAddress]
+        string Email
+    );
+
+}
